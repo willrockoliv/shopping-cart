df2992efebd33b505ef1cc3472deb00a17deff52
willrockoliv <william.r.o.wro@gmail.com>
2024-11-05 15:55:01 -0300

    test: adding unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
-from fastapi.testclient import TestClient
 import pytest
+from fastapi.testclient import TestClient
 
 from shopping_cart_api.app import app
+
 
 @pytest.fixture()
 def client():
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,27 +1,34 @@
-from http import HTTPStatus
 import json
+from http import HTTPStatus
+from unittest.mock import patch
+
+from fastapi import HTTPException
 from fastapi.testclient import TestClient
+import pytest
+
 from shopping_cart_api.app import OrderSchema
 
+
 def test_root(client: TestClient):
-    response = client.get('/')
+    response = client.get("/")
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hi! 👋"}
-    
+
+
 def test_create_order(client: TestClient):
     order = OrderSchema(
-        buyer_id = 1,
-        product_id = 1,
-        number_of_installments = 12,
-        total_amount = 10.000,
-        purchase_date = "2024-11-05",
+        buyer_id=1,
+        product_id=1,
+        number_of_installments=12,
+        total_amount=10.000,
+        purchase_date="2024-11-05",
     )
 
-    response = client.post(
-        "/order/",
-        json=json.loads(order.model_dump_json())
-    )
+    with patch("boto3.client") as mock_sns_client:
+        mock_sns_client.publish = {"MessageId": "11111", "SequenceNumber": "11111"}
+        response = client.post("/order/", json=json.loads(order.model_dump_json()))
+
     result_order = OrderSchema.model_validate(response.json())
 
     assert response.status_code == HTTPStatus.CREATED
@@ -33,3 +40,15 @@ def test_create_order(client: TestClient):
         and result_order.total_amount == order.total_amount
         and result_order.purchase_date == order.purchase_date
     )
+
+
+def test_create_order_failing(client: TestClient):
+    order = OrderSchema(
+        buyer_id=1,
+        product_id=1,
+        number_of_installments=12,
+        total_amount=10.000,
+        purchase_date="2024-11-05",
+    )
+    response = client.post("/order/", json=json.loads(order.model_dump_json()))
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
