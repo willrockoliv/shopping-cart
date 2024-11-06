fff0f608ae50d29ed926eb915f1bedc38225fc5a
William Rocha de Oliveira <william.r.o.wro@gmail.com>
2024-11-05 22:33:23 -0300

    cicd: adding env

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 from shopping_cart_api.app import app
 
 
-@pytest.fixture()
+@pytest.fixture
 def client():
     with TestClient(app) as client:
         yield client
